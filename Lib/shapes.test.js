018cfc2d90267f2631835f3f0a449c06ab363f44
Jadyn Gomez <jadyngg19@gmail.com>
2023-04-25 19:50:01 -0400

    Adding tests for shape backgrounds

---
+++ Lib/shapes.test.js
@@ -0,0 +1,31 @@
+const {Circle, Triangle, Square} = require("./shapes.js");
+
+describe("Circle Test", () => {
+    test("Test for a circle with a #2986cc background", () => {
+        const shape = new Circle();
+        shape.setColor("#2986cc");
+        expect(shape.render()).toEqual(
+            `<circle cx="150" cy="115" r="80" fill="#2986cc"/>`
+        );
+    });
+});
+
+describe("Triangle Test", () => {
+    test("Test for a traingle with a Green background", () => {
+        const shape = new Triangle();
+        shape.setColor("Green");
+        expect(shape.render()).toEqual(
+            `<polygon points="150, 18 244, 182 56, 182" fill="Green"/>`
+        );
+    });
+});
+
+describe("Square Test", () => {
+    test("Test for a square with a Pink background", () => {
+        const shape = new Square();
+        shape.setColor("Pink");
+        expect(shape.render()).toEqual(
+            `<rect x="73" y="40" width="160" height"160" fill="Pink"/>`
+        );
+    });
+});
